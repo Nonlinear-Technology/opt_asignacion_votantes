7e47a323a34ad081aa7cdcc655e56bff81098550
astablum <ASTABLUM@FUL.UNL.EDU.AR>
2025-05-05 11:30:40 -0300

    todolast

--- pages/5_Descripcion_del_problema.py
+++ pages/5_Descripcion_del_problema.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-# Configuración de la página
-st.set_page_config(page_title="Descripción del problema", layout="wide")
+st.set_page_config(page_title="Asignación votantes", page_icon=":material/home:", layout="wide", initial_sidebar_state="expanded")
+
+st.logo('Logo-normal.svg', icon_image='Logo-iso chico.svg')
 
 # Estilos visuales
 st.markdown("""
@@ -31,32 +32,31 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Título principal con emoji
+# Título principal con emoji de urna de votación
 st.markdown("<h1>🗳️ Descripción del problema</h1>", unsafe_allow_html=True)
 
-# Contenido formateado
+# Contenido con formato
 st.markdown("""
 <div class="highlight">
-En Argentina, la asignación de votantes a escuelas varía según el tipo de elección y el municipio. En ciudades grandes, como <strong>Santa Fe</strong>, se agrupa a los votantes por zonas electorales (<em>circuitos</em>) y luego se los distribuye entre las escuelas de cada zona.
+En Argentina, la asignación de votantes a escuelas varía según el tipo de elección y el municipio. En los más grandes, como <strong>Santa Fe</strong>, se agrupa a los votantes por zonas electorales (<em>circuitos</em>) y luego se los distribuye entre las escuelas de cada zona.
 </div>
 
-El método comúnmente utilizado consiste en <strong>ordenar alfabéticamente el padrón</strong> e ir asignando en ese mismo orden los votantes a cada mesa de cada escuela hasta completar su capacidad. Es decir, <strong>no hay ningún criterio geográfico</strong> en la asignación. De esta manera, muchas personas deben votar en <strong>escuelas alejadas</strong>, a pesar de tener otras más cercanas disponibles.
+Un método comúnmente utilizado consiste en <strong>ordenar alfabéticamente el padrón</strong> y asignar grupos de votantes a escuelas hasta completar su capacidad. Este procedimiento puede derivar en que algunas personas deban votar en <strong>escuelas alejadas</strong>, a pesar de tener otras más cercanas disponibles.
 
 <h2>Enfoque propuesto</h2>
 
-A partir de <strong>datos oficiales</strong>, desde <strong>Nonlinear</strong> proponemos un enfoque basado en <strong>ciencia de datos</strong> y <strong>optimización matemática</strong> para obtener una asignación que <strong>minimice las distancias recorridas</strong>, respetando las zonas electorales y las capacidades de las mesas (<em>Modelo “Óptimo Mesas Fijas”</em>).
+A partir de <strong>datos oficiales</strong>, desde <strong>Nonlinear</strong> proponemos un enfoque basado en <strong>rigurosidad matemática</strong> y <strong>herramientas informáticas</strong> para obtener una asignación óptima de votantes al colegio más cercano, respetando las zonas electorales y las capacidades de las mesas.
 
-Además, desarrollamos un modelo aún más eficiente (<em>“Óptimo Mesas Libres”</em>) que permite <strong>modificar la cantidad de mesas</strong> de los locales buscando aún mejores soluciones para los votantes.
+Además, desarrollamos un modelo aún más eficiente que permite <strong>adaptar la cantidad de mesas</strong> según la cercanía de las escuelas a la mayor concentración de votantes.
 
 <h2>Contenido del sitio</h2>
 
 En las siguientes páginas encontrarán:
 
 <ol>
-<li><strong>Resultados:</strong> Comparativa a nivel circuito (estos se pueden visualizar en <em>Ver circuitos</em>).</li>
+<li><strong>Comparativa de propuestas:</strong> Comparativa a nivel circuito (estos se pueden visualizar en <em>Ver circuitos</em>).</li>
 <li><strong>Ahorro global:</strong> Comparativa a nivel del sector electoral (Santa Fe Ciudad).</li>
 <li><strong>Nosotros:</strong> Información sobre la empresa.</li>
 <li><strong>Metodología:</strong> La ciencia detrás de la propuesta, junto con las hipótesis realizadas y otras observaciones.</li>
 </ol>
 """, unsafe_allow_html=True)
-
